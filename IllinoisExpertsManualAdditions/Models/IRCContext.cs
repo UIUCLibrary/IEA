853e90a5f87dd1dd524a4ca9da4210030db31f32
Michael Nelson <mikesweb@illinois.edu>
2022-02-14 15:11:58 -0600

    updated startup.cs to allow for specific error messages in a production environment to see the real error to fix.

--- IllinoisExpertsManualAdditions/Models/IRCContext.cs
+++ IllinoisExpertsManualAdditions/Models/IRCContext.cs
@@ -24,12 +24,13 @@ namespace IllinoisExpertsManualAdditions.Models
         public virtual DbSet<IrcPt1Job> IrcPt1Jobs { get; set; }
         public virtual DbSet<IrcPt2Urbprev> IrcPt2Urbprevs { get; set; }
 
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=lib-mssql-dev;Database=IRC;Trusted_Connection=True");
+                optionsBuilder.UseSqlServer("Server=lib-mssql-misc;Database=IRC;Trusted_Connection=True");
             }
         }
 
